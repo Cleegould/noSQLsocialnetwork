22172ebe4c0bf1ad9124f3d32fd72f385310da0f
chrisg <cleegould@gmail.com>
2023-06-01 18:40:07 -0400

    finally

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
 
 const app = express();
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(routes);
 
-mongoose.connect(process.env.MONGOB_URI || 'mongodb://localhost:27017/', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
   });
-  
-  mongoose.set('debug', true);
-  
-  app.use(require('./routes'));
-  
-  app.listen(PORT, () => console.log(`You have been connected on localhost:${PORT}`));
+});
